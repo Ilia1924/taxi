bd0f03d68463e924f9123699781b4c9d4072a198
Илья Кочетков <105704600+Ilia1924@users.noreply.github.com>
2024-12-03 14:56:05 +0000

    add mock

---
+++ app/components/screen/home/data.ts
@@ -0,0 +1,51 @@
+import economyIcon from '../../../accets/images/options/taxi.png';
+import busIcon from '../../../accets/images/options/charter.png';
+import comfortIcon from '../../../accets/images/options/minivan-taxi.png';
+import partyBusIcon from '../../../accets/images/options/taxibus.png';
+import limusinIcon from '../../../accets/images/options/limousine.png';
+
+interface IList {
+    id: string,
+    title: string,
+    img: string,
+    multiplier: number
+}
+
+export const optionList: IList[] = [
+    {
+        id: 'econom-1',
+        title: "Economy",
+        img: economyIcon.src,
+        multiplier: 90
+    }, 
+    {
+        id: 'comfort-1',
+        title: "Comfort",
+        img: comfortIcon.src,
+        multiplier: 60
+    }, 
+    {
+        id: 'bus-1',
+        title: "Bus",
+        img: busIcon.src,
+        multiplier: 40
+    }, 
+    {
+        id: 'partybus-1',
+        title: "Party Bus",
+        img: partyBusIcon.src,
+        multiplier: 60
+    }, 
+    {
+        id: 'limusin-1',
+        title: "LUX",
+        img: limusinIcon.src,
+        multiplier: 140
+    }, 
+    // {
+    //     id: 'econom-1',
+    //     title: "Economy",
+    //     img: economyIcon.src,
+    //     multiplier: 90
+    // }, 
+]
